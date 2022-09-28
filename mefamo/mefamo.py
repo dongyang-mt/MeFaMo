de21b54067db21d366d3b954bbea567ca1a9ea19
dong.yang <dong.yang@mthreads.com>
2022-09-28 08:14:26 +0000

    comment open3d code for mtGPU free(): invalid pointer Aborted (core dumped)

--- mefamo/mefamo.py
+++ mefamo/mefamo.py
@@ -8,11 +8,11 @@ import threading
 import time
 import math
 import transforms3d
-import open3d as o3d
+# import open3d as o3d
 
 from pylivelinkface import PyLiveLinkFace, FaceBlendShape
 
-from mefamo.utils.drawing import Drawing
+# from mefamo.utils.drawing import Drawing
 from mefamo.blendshapes.blendshape_calculator import BlendshapeCalculator
 
 # taken from: https://github.com/Rassibassi/mediapipeDemos
@@ -67,7 +67,7 @@ def calculate_rotation(face_landmarks, pcf: PCF, image_shape):
 
     return pose_transform_mat, metric_landmarks, rotation_vector, translation_vector
 
-   
+
 class Mefamo():
     def __init__(self, input = 0, ip = '127.0.0.1', port = 11111, show_3d = False, hide_image = False, show_debug = False) -> None:
 
@@ -87,7 +87,7 @@ class Mefamo():
 
         self.ip = ip
         self.upd_port = port
-        
+
         self.image_height, self.image_width, channels = (480, 640, 3)
 
         # pseudo camera internals
@@ -105,38 +105,38 @@ class Mefamo():
             frame_width=self.image_width,
             fy=camera_matrix[1, 1],
         )
-        self.drawing_spec = drawing_utils.DrawingSpec(thickness=1, circle_radius=1)        
+        self.drawing_spec = drawing_utils.DrawingSpec(thickness=1, circle_radius=1)
         self.lock = threading.Lock()
         self.got_new_data = False
         self.network_data = b''
         self.network_thread = threading.Thread(target=self._network_loop, daemon=True)
         self.image = None
-    
+
     # starts the program and all its threads
-    def start(self):        
+    def start(self):
         cap = None
         image = None
 
-        # check if input is an image        
+        # check if input is an image
         if isinstance(self.input, str) and (self.input.lower().endswith(".jpg") or self.input.lower().endswith(".png")):
             image = cv2.imread(self.input)
-            self.file = True   
-        else:   
-            input = self.input  
+            self.file = True
+        else:
+            input = self.input
             try:
                 input = int(self.input)
             except ValueError:
-                input = self.input  
+                input = self.input
 
         if os.name == 'nt':
-            # will improve webcam input startup on windows 
+            # will improve webcam input startup on windows
             cap = cv2.VideoCapture(input)
         else:
-            cap = cv2.VideoCapture(input)                
+            cap = cv2.VideoCapture(input)
 
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.image_width)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.image_height)
-        
+
         # run the network loop in a separate thread
         self.network_thread.start()
 
@@ -148,10 +148,10 @@ class Mefamo():
                     print("Ignoring empty camera frame.")
                     continue
                 if not self._process_image(image):
-                    break    
-            print("Video capture received no more frames.")                
+                    break
+            print("Video capture received no more frames.")
             cap.release()
-        
+
         else:
             # for input images
             while image is not None:
@@ -159,16 +159,16 @@ class Mefamo():
                     break
 
     def _network_loop(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:            
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
             s.connect((self.ip, self.upd_port))
-            while True: 
+            while True:
                 with self.lock:
-                    if self.got_new_data:                               
+                    if self.got_new_data:
                         s.sendall(self.network_data)
                         self.got_new_data = False
                 time.sleep(0.01)
 
-    def _process_image(self, image):   
+    def _process_image(self, image):
         # To improve performance, optionally mark the image as not writeable to
         # pass by reference.
         image.flags.writeable = False
@@ -182,12 +182,12 @@ class Mefamo():
         if results.multi_face_landmarks:
             for face_landmarks in results.multi_face_landmarks:
 
-                pose_transform_mat, metric_landmarks, rotation_vector, translation_vector = calculate_rotation(face_landmarks, self.pcf, image.shape)  
+                pose_transform_mat, metric_landmarks, rotation_vector, translation_vector = calculate_rotation(face_landmarks, self.pcf, image.shape)
                 # draw a 3d image of the face
                 if self.show_3d:
                     face_image_3d = Drawing.draw_3d_face(metric_landmarks, image)
 
-                # draw the face mesh 
+                # draw the face mesh
                 drawing_utils.draw_landmarks(
                     image=image,
                     landmark_list=face_landmarks,
@@ -204,12 +204,12 @@ class Mefamo():
                     landmark_drawing_spec=None,
                     connection_drawing_spec=drawing_styles
                     .get_default_face_mesh_contours_style())
-            
-                 # draw iris points
-                image = Drawing.draw_landmark_point(face_landmarks.landmark[468], image, color = (0, 0, 255))
-                image = Drawing.draw_landmark_point(face_landmarks.landmark[473], image, color = (0, 255, 0))
 
-                # calculate and set all the blendshapes                
+                #  # draw iris points
+                # image = Drawing.draw_landmark_point(face_landmarks.landmark[468], image, color = (0, 0, 255))
+                # image = Drawing.draw_landmark_point(face_landmarks.landmark[473], image, color = (0, 255, 0))
+
+                # calculate and set all the blendshapes
                 self.blendshape_calulator.calculate_blendshapes(
                     self.live_link_face, metric_landmarks[0:3].T, face_landmarks.landmark)
 
@@ -235,12 +235,12 @@ class Mefamo():
         color = (0, 255, 0)
 
         if self.show_image:
-            cv2.imshow('MediaPipe Face Mesh', image.astype('uint8'))  
-            if face_image_3d is not None and type(face_image_3d) == o3d.geometry.Image: 
+            cv2.imshow('MediaPipe Face Mesh', image.astype('uint8'))
+            if face_image_3d is not None and type(face_image_3d) == o3d.geometry.Image:
                 # show the 3d image if it exists
                 img_3d = np.asarray(face_image_3d)
                 img_3d = cv2.flip(img_3d, 1)
-                cv2.imshow('Open3D Image', np.asarray(face_image_3d)) 
+                cv2.imshow('Open3D Image', np.asarray(face_image_3d))
 
             if self.show_debug:
                 for shape in FaceBlendShape:
